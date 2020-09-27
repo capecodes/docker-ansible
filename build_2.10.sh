775ba7306b83541f379d9bf584e7abf9be1d96c3
Cape Codes <info@cape.codes>
2020-09-27 17:29:15 -0400

    initial Ansible 2.10 tests (still WIP, not fully tested)

---
+++ build_2.10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# (C) 2017, Cape Codes, <info@cape.codes>
+# Dual licensed with MIT and GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# see https://pypi.python.org/pypi/ansible
+
+MAJOR_VERSION=2.10
+EXACT_VERSION=2.10.0
+
+./build.sh ${EXACT_VERSION} Dockerfile_2.10_up
+# ./push.sh ${MAJOR_VERSION} ${EXACT_VERSION}
+
+# MAJOR_VERSION=latest
+
+# ./push.sh ${MAJOR_VERSION} ${EXACT_VERSION}
