d9fdb9cd1954f3b31ba52ad28707bb7cb1da47c9
Cape Codes <info@cape.codes>
2020-10-03 14:46:53 -0400

    updated for python3 compliance with io.StringIO

--- x_stdout_json_lines.py
+++ x_stdout_json_lines.py
@@ -2,7 +2,7 @@
 # Dual licensed with MIT and GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
-from cStringIO import StringIO
+from io import StringIO
 
 __metaclass__ = type
 
